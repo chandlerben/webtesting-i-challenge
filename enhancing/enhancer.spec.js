e43d6bdb2c285a9cc7f30c6c3a1940e223fbbbed
benjchand <benchand@gmail.com>
2019-05-13 13:51:55 -0700

    Done with Repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,45 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+/*  
+{
+    'name': 'Sword of Truth',
+    'enhancement': 17,
+    'durability': 96
+
+}
+
+*/
+
+describe("enhancer.js", () => {
+  describe("succeed", () => {});
+
+  describe("fail", () => {});
+
+  describe("repair", () => {
+    it("should repair the item", () => {
+      expect(enhancer.repair(brokenItem)).toStrictEqual(repairedItem);
+      expect(enhancer.repair(reallyBrokenItem)).toStrictEqual(repairedItem);
+    });
+  });
+
+  describe("get", () => {});
+});
+
+const brokenItem = {
+  name: "Sword of Truth",
+  enhancement: 17,
+  durability: 96
+};
+
+const reallyBrokenItem = {
+  name: "Sword of Truth",
+  enhancement: 17,
+  durability: 0
+};
+
+const repairedItem = {
+  name: "Sword of Truth",
+  enhancement: 17,
+  durability: 100
+};
